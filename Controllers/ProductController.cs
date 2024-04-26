b099e68788cf2adaf18749d14e00f9b277ef862e
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-26 13:24:24 +0100

    Création d’un contrôleur

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP08.Models;
+
+namespace TP08.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product produit = new Product()
+            {
+                Id = 1,
+                Name = "Produit 1",
+                Price = 10.5m
+            };
+            List<Product> produits = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = 2,
+                    Name = "Produit 2",
+                    Price = 20.5m
+                },
+               new Product()
+               {
+                   Id = 3,
+                   Name = "Produit 3",
+                   Price = 30.5m
+               }
+            };
+            return View(produits);
+        }
+    }
+}
